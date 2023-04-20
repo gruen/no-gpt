a26f5759802475f1e788a20e96920ad5a97bad58
Michael E. Gruen <mg@exposure.co>
2023-04-20 14:53:21 -0400

    functional enough.

--- content.js
+++ content.js
@@ -13,11 +13,11 @@ function filterGPTMessages(messages) {
     const filteredMessages = messages.filter(message => {
         const lowerCaseMessage = message.toLowerCase();
         const gptPhrases = [
-            // "the",
-              "i asked gpt to write",
-              "i requested gpt to write",
-              "i got gpt to write",
-              "i had gpt write",
+            "the",
+            //   "i asked gpt to write",
+            //   "i requested gpt to write",
+            //   "i got gpt to write",
+            //   "i had gpt write",
         ];
 
         return !gptPhrases.some(phrase => lowerCaseMessage.includes(phrase));
@@ -26,23 +26,31 @@ function filterGPTMessages(messages) {
     return filteredMessages;
 }
 
-function toggleMessageDisplay(container, message) {
-    if (filterEnabled && !filterGPTMessages([message]).length) {
-        container.style.display = 'none';
-    } else {
-        container.style.display = '';
+function toggleMessageDisplay(container, message, platform) {
+    if (platform === 'linkedin') {
+      container = container.closest('.feed-shared-update-v2');
     }
-}
+  
+    if (filterEnabled && !filterGPTMessages([message]).length) {
+      container.style.display = 'none';
+    } else {
+      container.style.display = '';
+    }
+  }
 
 function filterPageMessages() {
-    const linkedInMessageContainers = document.querySelectorAll('.feed-shared-update-v2__description'); // LinkedIn selector
-    const twitterMessageContainers = document.querySelectorAll('article div[lang]:not([style])'); // Twitter selector
-    const messageContainers = [...linkedInMessageContainers, ...twitterMessageContainers];
+  const linkedInMessageContainers = document.querySelectorAll('.feed-shared-update-v2__description'); // LinkedIn selector
+  const twitterMessageContainers = document.querySelectorAll('article div[lang]:not([style])'); // Twitter selector
 
-    messageContainers.forEach(container => {
-        const message = container.innerText || container.textContent;
-        toggleMessageDisplay(container, message);
-    });
+  linkedInMessageContainers.forEach(container => {
+    const message = container.innerText || container.textContent;
+    toggleMessageDisplay(container, message, 'linkedin');
+  });
+
+  twitterMessageContainers.forEach(container => {
+    const message = container.innerText || container.textContent;
+    toggleMessageDisplay(container, message, 'twitter');
+  });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -66,3 +74,4 @@ const observerCallback = (mutationsList, observer) => {
 const observer = new MutationObserver(observerCallback);
 observer.observe(targetNode, config);
 
+filterPageMessages();
